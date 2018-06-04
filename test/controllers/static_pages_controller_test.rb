934a697f33a7152b8770da1b8ce93f8b5091ee58
tienthuan1412 <tienthuan1412@gmail.com>
2018-06-04 18:08:53 +0700

    make a basic User model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,32 +3,21 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-<<<<<<< HEAD
-    get static_pages_home_url
-=======
     get root_path
->>>>>>> filling-in-layout
     assert_response :success
     assert_select "title", "Ruby on rails Tutorial Sample App"
   end
 
   test "should get help" do
-<<<<<<< HEAD
-    get static_pages_help_url
-=======
-    get help_path
->>>>>>> filling-in-layout
+    get helf_path
     assert_response :success
     assert_select "title", "Help | Ruby on rails Tutorial Sample App"
   end
 
-<<<<<<< HEAD
-=======
   test "should get contact" do
     get contact_path
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
->>>>>>> filling-in-layout
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,7 +4,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
     get root_path
-    assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", helf_path
     assert_select "a[href=?]", contact_path
